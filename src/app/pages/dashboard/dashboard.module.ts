a78f839f2c6b027003c895501154f93b5bcd1b51
Raja <raja@twilightsoftwares.com>
2021-01-25 19:30:33 +0530

    Initial commit

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PatientsComponent } from './patients/patients.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, PatientsComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
