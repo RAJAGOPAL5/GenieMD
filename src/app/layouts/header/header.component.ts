46f2486a0a5763cc43409af25d02aa5c4ae37e23
jeevadhasan <jeevadhasan@twilightsoftwares.com>
2021-02-17 20:39:23 +0530

    fix: header profile

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { DashboardService } from 'src/app/shared/services/dashboard.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +12,19 @@ export class HeaderComponent implements OnInit {
   modalRef!: NgbModalRef;
 
 
-  constructor() { }
-
+  constructor(private dashboardService: DashboardService, private route: ActivatedRoute) { }
+  userID: any;
+  profileName = ''
   ngOnInit(): void {
+    this.route.params.subscribe(item => {
+      this.userID = item.userID;
+    });
+    this.getprofile();
   }
-
+  getprofile(){
+    this.dashboardService.getProfile(this.userID).subscribe((data: any) => {
+      console.log(data)
+      this.profileName = data.userName
+    })
+  }
 }
