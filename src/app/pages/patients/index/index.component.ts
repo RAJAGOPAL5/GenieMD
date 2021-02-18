97805d049315ae16532e141204c1c6ea0ce36703
Raja <raja@twilightsoftwares.com>
2021-02-18 16:56:14 +0530

    Fix:  pages name

--- src/app/pages/patients/index/index.component.ts
+++ src/app/pages/patients/index/index.component.ts
@@ -7,16 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
   tabs = [
+    // {
+    //   title: 'Route tab #1',
+    //   route: '/pages/description',
+    //   icon: 'home',
+    //   responsive: true, // hide title before `route-tabs-icon-only-max-width` value
+    // },
+    // {
+    //   title: 'Route tab #2',
+    //   route: '/pages/images',
+    // },
     {
-      title: 'Route tab #1',
-      route: '/pages/description',
-      icon: 'home',
-      responsive: true, // hide title before `route-tabs-icon-only-max-width` value
+      title: 'Vitals #3',
+      route: '/pages/',
     },
     {
-      title: 'Route tab #2',
-      route: '/pages/images',
-    }
+      title: 'Alerts #4',
+      route: '/pages/',
+    },
+    {
+      title: 'Visits #5',
+      route: '/pages/',
+    },
+    {
+      title: 'Care Team #6',
+      route: '/pages/',
+    },
+    {
+      title: 'Profile #7',
+      route: '/pages/',
+    },
+    {
+      title: 'History #8',
+      route: '/pages/',
+    },
+
   ];
   constructor() { }
 
