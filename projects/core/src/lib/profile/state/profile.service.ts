8ce0469e903000121ae67926132e9f8b32169449
Priyadharshini <priyadharshini@twilightits.com>
2021-02-23 13:11:54 +0530

    feat: Added interfaces

--- projects/core/src/lib/profile/state/profile.service.ts
+++ projects/core/src/lib/profile/state/profile.service.ts
@@ -8,6 +8,17 @@ export class ProfileService {
 
   constructor(private profileStore: ProfileStore, private http: HttpClient) {
   }
-
+  getUser(id: string) {
+    return this.http.get(`Profile/${id}`);
+  }
+  sendEmail(payload) {
+    return this.http.post(`system/SendEmail`, payload);
+  }
+  updateProfile(payload) {
+    return this.http.post(`Profile/Update`, payload);
+  }
+  addPatient(payload) {
+    return this.http.post(`Clinics/AddPatient`, payload);
+  }
 
 }
