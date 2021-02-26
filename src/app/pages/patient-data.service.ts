1a5f8e7809697c465b35411f297e976f5b617030
Singaravelavane Pougajendy <singaravelavane@twilightsoftwares.com>
2021-02-26 18:02:12 +0530

    create:  service for the language, state and morbidity

---
+++ src/app/pages/patient-data.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientDataService {
+  languages: any[] = [];
+  states: any[] = [];
+  morbidity: any[] = [];
+  constructor() { }
+  getLanguages() {
+   return this.languages = [{ name: 'English', id: 1 },
+    { name: 'Spanish', id: 2 },
+    { name: 'Chinese', id: 3 },
+    { name: 'Japanese', id: 4 },
+    { name: 'Russian', id: 5 },
+    { name: 'Portuguese', id: 6 },
+    { name: 'French', id: 7 },
+    { name: 'Italian', id: 8 },
+    { name: 'Arabic', id: 9 },
+    { name: 'German', id: 10 },
+    { name: 'Korean', id: 11 }
+    ];
+  }
+  getState() {
+   return this.states = [{ name: 'Alabama', abbreviation: 'AL' },
+    { name: 'Alaska', abbreviation: 'AK' },
+    { name: 'Arizona', abbreviation: 'AZ' },
+    { name: 'Arkansas', abbreviation: 'AR' },
+    { name: 'California', abbreviation: 'CA' },
+    { name: 'Colorado', abbreviation: 'CO' },
+    { name: 'Connecticut', abbreviation: 'CT' },
+    { name: 'District Of Columbia', abbreviation: 'DC' },
+    { name: 'Delaware', abbreviation: 'DE' },
+    { name: 'Florida', abbreviation: 'FL' },
+    { name: 'Georgia', abbreviation: 'GA' },
+    { name: 'Hawaii', abbreviation: 'HI' },
+    { name: 'Idaho', abbreviation: 'ID' },
+    { name: 'Illinois', abbreviation: 'IL' },
+    { name: 'Indiana', abbreviation: 'IN' },
+    { name: 'Iowa', abbreviation: 'IA' },
+    { name: 'Kansas', abbreviation: 'KS' },
+    { name: 'Kentucky', abbreviation: 'KY' },
+    { name: 'Louisiana', abbreviation: 'LA' },
+    { name: 'Maine', abbreviation: 'ME' },
+    { name: 'Marshall Islands', abbreviation: 'MH' },
+    { name: 'Maryland', abbreviation: 'MD' },
+    { name: 'Massachusetts', abbreviation: 'MA' },
+    { name: 'Michigan', abbreviation: 'MI' },
+    { name: 'Minnesota', abbreviation: 'MN' },
+    { name: 'Mississippi', abbreviation: 'MS' },
+    { name: 'Missouri', abbreviation: 'MO' },
+    { name: 'Montana', abbreviation: 'MT' },
+    { name: 'Nebraska', abbreviation: 'NE' },
+    { name: 'Nevada', abbreviation: 'NV' },
+    { name: 'New Hampshire', abbreviation: 'NH' },
+    { name: 'New Jersey', abbreviation: 'NJ' },
+    { name: 'New Mexico', abbreviation: 'NM' },
+    { name: 'New York', abbreviation: 'NY' },
+    { name: 'North Carolina', abbreviation: 'NC' },
+    { name: 'North Dakota', abbreviation: 'ND' },
+    { name: 'Northern Mariana Islands', abbreviation: 'MP' },
+    { name: 'Ohio', abbreviation: 'OH' },
+    { name: 'Oklahoma', abbreviation: 'OK' },
+    { name: 'Oregon', abbreviation: 'OR' },
+    { name: 'Palau', abbreviation: 'PW' },
+    { name: 'Pennsylvania', abbreviation: 'PA' },
+    { name: 'Puerto Rico', abbreviation: 'PR' },
+    { name: 'Rhode Island', abbreviation: 'RI' },
+    { name: 'South Carolina', abbreviation: 'SC' },
+    { name: 'South Dakota', abbreviation: 'SD' },
+    { name: 'Tennessee', abbreviation: 'TN' },
+    { name: 'Texas', abbreviation: 'TX' },
+    { name: 'Utah', abbreviation: 'UT' },
+    { name: 'Vermont', abbreviation: 'VT' },
+    { name: 'Virgin Islands', abbreviation: 'VI' },
+    { name: 'Virginia', abbreviation: 'VA' },
+    { name: 'Washington', abbreviation: 'WA' },
+    { name: 'West Virginia', abbreviation: 'WV' },
+    { name: 'Wisconsin', abbreviation: 'WI' },
+    { name: 'Wyoming', abbreviation: 'WY' }
+    ];
+  }
+  getMorbidity() {
+    return this.morbidity = [{ name: 'Lung Disease', id: 0 }, { name: 'Heart Disease', id: 1 }];
+  }
+}
