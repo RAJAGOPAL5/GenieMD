1e1c0a1242adcd92b0a66caae6556a6a20573840
Vasanth Jagadeesan <vasanth@twilightsoftwares.com>
2021-03-03 17:45:02 +0530

    fix: add preload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { ClinicConfigResolver } from './shared/resolvers /clinic-config.resolver';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { ProfileResolve } from './shared/resolvers /profile.resolve';
 
 
@@ -25,7 +25,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
