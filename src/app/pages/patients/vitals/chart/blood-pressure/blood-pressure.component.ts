aefe387fa5316e944bcb5765d0a2a25c4c500270
Priyadharshini <priyadharshini@twilightits.com>
2021-03-04 15:22:03 +0530

    fix: Color changes in chart

--- src/app/pages/patients/vitals/chart/blood-pressure/blood-pressure.component.ts
+++ src/app/pages/patients/vitals/chart/blood-pressure/blood-pressure.component.ts
@@ -70,6 +70,11 @@ export class BloodPressureComponent implements OnInit {
        }
       }]
     },
+    legend: {
+      labels: {
+        usePointStyle: true
+      }
+    },
     elements:
     {
       point:
