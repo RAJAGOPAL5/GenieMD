0c61b38ee4de2a6787e088d0afe737fe14a75b37
Raja <raja@twilightsoftwares.com>
2021-03-10 20:56:07 +0530

    fix: device module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NbDialogModule, NbLayoutModule, NbThemeModule, NbIconModule } from '@nebular/theme';
+import { NbDialogModule, NbLayoutModule, NbThemeModule, NbIconModule, NbInputModule } from '@nebular/theme';
 import { MissingTranslationHandler, MissingTranslationHandlerParams, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -20,6 +20,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     NbDialogModule.forChild(),
     NbIconModule,
     ReactiveFormsModule,
+    NbInputModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
