dc40eddc78e3f9165b35b30e00c0b88e5a65615d
Raja <raja@twilightsoftwares.com>
2021-03-13 16:09:11 +0530

    fix: notifycation and patient service state.

--- projects/core/src/lib/service/notification/state/notification.service.ts
+++ projects/core/src/lib/service/notification/state/notification.service.ts
@@ -8,8 +8,25 @@ export class NotificationService {
 
   constructor(private notificationStore: NotificationStore, private http: HttpClient) {
   }
-  notifyOptions(data) {
-    return this.http.post(`Notifications/NotifyUsersWithOptions`, data);
+  async notifyOptions(data) {
+    try {
+      this.notificationStore.setLoading(true);
+      await this.http.post(`Notifications/NotifyUsersWithOptions`, data)
+      .pipe(
+        tap((response: any) => {
+          console.log ('notifyOptions result:', response);
+          this.notificationStore.update({
+            notifyInfo : response,
+          })
+        })
+      ).toPromise();
+    }catch(error) {
+      console.log('notifyOption', error.statusText)
+      this.notificationStore.setError(error);
+    }
   }
+  // notifyOptions(data) {
+  //   return this.http.post(`Notifications/NotifyUsersWithOptions`, data);
+  // }
 
 }
