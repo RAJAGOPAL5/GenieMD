236e0771b1e33c7d08bf3639b39c032b359ac56a
Vasanth Jagadeesan <vasanth@twilightsoftwares.com>
2021-03-19 16:43:30 +0530

    fix: removed unused files

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,24 +4,22 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { IndexComponent } from './index/index.component';
 import { SharedModule } from '../shared/shared.module';
 import { NbIconModule, NbMenuModule, NbRouteTabsetModule, NbSidebarModule, NbUserModule, NbSelectModule,
-   NbButtonModule, NbThemeModule, NbTooltipModule, NbButtonGroupModule } from '@nebular/theme';
+   NbButtonModule, NbThemeModule, NbTooltipModule, NbButtonGroupModule, NbLayoutModule } from '@nebular/theme';
 import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [IndexComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
-    SharedModule,
     NbRouteTabsetModule,
+    NbLayoutModule,
     NbSidebarModule,
     NbMenuModule,
     NbIconModule,
     NbUserModule,
-    FormsModule,
     NbButtonModule,
     NbThemeModule,
     NbTooltipModule,
-    NbButtonGroupModule
   ]
 })
 export class PagesModule { }
